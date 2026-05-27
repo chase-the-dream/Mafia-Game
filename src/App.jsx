0a12f980c115089c3263e0a7e7475587383222b2
Chase <ch517661@ucf.edu>
2026-05-27 14:29:50 -0400

    Hash Debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 // src/App.jsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './Components/HomePage'
 import SetupPage from './Components/SetupPage'
 import GamePage from './Components/GamePage'
 
 function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/setup" element={<SetupPage />} />
         <Route path="/game" element={<GamePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
